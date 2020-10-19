76dbb1414fad54a7cb659104e256dad6b0f4e71f
wata14341 <akki.ni.ni.32@gmail.com>
2020-10-20 06:41:39 +0900

    update drawer

--- src/components/humberger.js
+++ src/components/humberger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 class Humberger extends React.Component {
     constructor() {
@@ -19,11 +20,11 @@ class Humberger extends React.Component {
     }
 
     render() {
-        let drawer;
-        if (this.state.isOpen){
-            drawer = (
-                <div class="drawer-out">
-                    <div class="links">
+        return(
+            <div id="buger-menu">
+                <CSSTransition in={this.state.isOpen} timeout={400} classNames="drawer">
+                    <div className="drawer">
+                    <div className="links">
                         <Link to='/' class="link">Home</Link>
                         <br />
                         <Link to='/practice' class="link">Practice</Link>
@@ -31,18 +32,14 @@ class Humberger extends React.Component {
                         <Link to='/contact' class="link">Contact</Link>
                         <br />
                     </div>
-                </div>
-            );
-        }
-        return(
-            <div id="buger-menu">
-                <button type="button" id="buttonHamburger" class="button hamburger" onClick={() => {this.handleOpen()}}>
-                    <span class="hamburger_line">
+                    </div>
+                </CSSTransition>
+                <button type="button" id="buttonHamburger" className="button hamburger" onClick={() => {this.handleOpen()}}>
+                    <span className="hamburger_line">
                          {/*<span class="visuallyHidden">
                          </span>*/}
                     </span>
                 </button>
-                { drawer }
             </div>
         );
     }
