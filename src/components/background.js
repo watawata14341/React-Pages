80961b93815101267077189555f5f00837a970af
wata14341 <akki.ni.ni.32@gmail.com>
2020-10-26 23:00:23 +0900

    add bg

--- src/components/background.js
+++ src/components/background.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Sakura from '../assets/sakura.svg'
 
 class Background extends React.Component {
     render() {
         return (
             <section className="bg">
+                <img src={ Sakura } width="20" height="20" alt="桜" className="sakura"/>
                 <div className="snow1"></div>
                 <div className="snow2"></div>
                 <div className="snow3"></div>
