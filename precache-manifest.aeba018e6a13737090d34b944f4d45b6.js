15f228ff660a92cf5bdb24f0c530dadcc845882e
wata14341 <akki.ni.ni.32@gmail.com>
2020-10-30 22:42:32 +0900

    Updates

---
+++ precache-manifest.aeba018e6a13737090d34b944f4d45b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7010f1820b2fb5af6605bc8cc1a2400e",
+    "url": "/React-demo/index.html"
+  },
+  {
+    "revision": "6ceb046f07202e9b740b",
+    "url": "/React-demo/static/css/main.9c95957c.chunk.css"
+  },
+  {
+    "revision": "9ed635b4ba35626e3ada",
+    "url": "/React-demo/static/js/2.86370ad3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-demo/static/js/2.86370ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ceb046f07202e9b740b",
+    "url": "/React-demo/static/js/main.2819e32b.chunk.js"
+  },
+  {
+    "revision": "e7d1348adb93cbca3085",
+    "url": "/React-demo/static/js/runtime-main.c5f05ef1.js"
+  },
+  {
+    "revision": "e3ceb55cb22d1704991d8d223f8f3432",
+    "url": "/React-demo/static/media/image-icon.e3ceb55c.jpg"
+  },
+  {
+    "revision": "1d2abae62ad9027e4e3735b303c0eee6",
+    "url": "/React-demo/static/media/momiji.1d2abae6.svg"
+  },
+  {
+    "revision": "0b8c4678ed0e8f6f2cebd2f71437b828",
+    "url": "/React-demo/static/media/sakura.0b8c4678.svg"
+  }
+]);
