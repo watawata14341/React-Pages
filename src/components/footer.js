fc08061d08b61842cca493fad8e151b594c943b1
wata14341 <akki.ni.ni.32@gmail.com>
2021-04-20 19:51:54 +0900

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <p>© 2020 Akihiko Watanuki. All rights reserved.</p>
+                <p>© 2021 Akihiko Watanuki. All rights reserved.</p>
             </footer>
         );
     }
